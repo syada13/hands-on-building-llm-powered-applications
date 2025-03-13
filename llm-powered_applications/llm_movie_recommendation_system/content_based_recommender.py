f7991c345dbfa74f846dfb978386ef31961aa4ba
syadav <suresh23_himcs@rediffmail.com>
2025-03-13 13:00:07 +0530

    Define the parametrized prompt chunks

--- llm-powered_applications/llm_movie_recommendation_system/content_based_recommender.py
+++ llm-powered_applications/llm_movie_recommendation_system/content_based_recommender.py
@@ -13,17 +13,53 @@ def user_dataset():
     # Convert the "movies" column into dictionaries
     for i, row_movies in enumerate(data["movies"]):
         movie_dict={}
-    for movie, rating in row_movies:
-        movie_dict[movie] = rating
-        data["movies"][i] = movie_dict
+        for movie, rating in row_movies:
+            movie_dict[movie] = rating
+            data["movies"][i] = movie_dict
 
-        # Create a pandas DataFrame
-        df = pd.DataFrame(data)
-        print(df.head())
+    # Create a pandas DataFrame
+    df = pd.DataFrame(data)
+    print(df.head())
+    "\n"
+
+   #Define the parametrized prompt chunks:
+    template_prefix = """You are a movie recommender system that help users to find movies that match their preferences.
+    Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    {context}
+    """
+
+    user_info = """This is what we know about the user, and you can use this information to better tune your research:
+    Age: {age}
+    Gender: {gender}
+    """
+
+    template_suffix = """
+    Question: {question}
+    Your response:
+    """
+
+    age = df.loc[df['username']=='Alice'] ['age'][0]
+    gender = df.loc[df['username'] == 'Alice']['gender'][0]
+    movies = ''
+    # Iterate over the dictionary and output movie name and rating
+    for movie,rating in df["movies"][0].items():
+        output_string = f"Movie: {movie}, Rating: {rating}" + "\n"
+        movies = movies +output_string
+        print(output_string)
+
+    user_info = user_info.format(age=18, gender='female')
+    combined_template = template_prefix + '\n' + user_info + '\n' + template_suffix
+    print(combined_template)
+
+
 
 
 if __name__ == "__main__":
     user_dataset()
+
+
+
 
 
 
