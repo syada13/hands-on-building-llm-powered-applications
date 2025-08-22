93b31e365830f6fac7e271bef74a81616e02c0e0
Suresh Yadav <suresh23_himcs@rediffmail.com>
2025-08-22 21:41:22 +0530

    Create a custom tool from Runnable

--- agents/custom_tool.py
+++ agents/custom_tool.py
@@ -33,3 +33,36 @@ def calculator(expression:str) -> str:
   agent= create_react_agent(llm,[calculator])
   for event in agent.stream({"messages":[("user", query)]},stream_mode="values"):
     event["messages"][-1].pretty_print()
+
+
+""" Creating a tool from a Runnable.
+ 1. Define calculator function 
+ 2. Convert calculator function to Runnable using RunnableLambda
+ 3. Convert runnable to tool
+"""
+
+from langchain_core.runnables import RunnableLambda
+from langchain_core.tools import convert_runnable_to_tool
+
+def calculator(expression:str) -> str:
+  math_constants = {"pi": math.pi, "i": 1j, "e": math.exp}
+  result = numerical_expression.evaluate(expression.strip ,local_dict=math_constants)
+  return str(result)
+
+calculator_with_retry = RunnableLambda(calculator).with_retry(
+  wait_exponential_jitter=True,
+   stop_after_attempt=3,
+)
+
+calculator_tool = convert_runnable_to_tool(
+  calculator_with_retry,
+  name="calculator",
+  description=(
+     "Calculates a single mathematical expression, incl. complex numbers."
+     "'\nAlways add * to operations, examples:\n73i -> 73*i\n"
+     "7pi**2 -> 7*pi**2"),
+     arg_types={"expression": "str"}
+)
+
+#Test
+print(llm.invoke("How much is (2+3i)**2", tools=[calculator_tool]).tool_calls[0])
